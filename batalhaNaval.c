a4b0d9f612a0f01515d09514975a7c390f8ea118
WillianSMS <202504483901@alunos.estacio.br>
2025-07-28 11:02:25 +0000

    adição de #define e adcionando um novo navio

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,42 +1,53 @@
 #include <stdio.h>
 
+// Definições de constantes
+#define TAMANHO 10
+#define NAVIO 3
+#define AGUA 0
+
 int main() {
-    int tabuleiro[10][10] = {0}; // Tabuleiro 10x10 inicializado com 0 (água)
+    int tabuleiro[TAMANHO][TAMANHO];
+    int i, j;
 
-    // Coordenadas iniciais dos navios
-    int linha_horizontal = 2, coluna_horizontal = 4; // Navio horizontal em (2,4)
-    int linha_vertical = 5, coluna_vertical = 1;     // Navio vertical em (5,1)
-
-    int i;
-
-    // Posiciona navio horizontal (tamanho 3)
-    for (i = 0; i < 3; i++) {
-        if (coluna_horizontal + i < 10 &&
-            tabuleiro[linha_horizontal][coluna_horizontal + i] == 0) {
-            tabuleiro[linha_horizontal][coluna_horizontal + i] = 3;
+    // Inicializa todas as posições com 0 (representando água)
+    for (i = 0; i < TAMANHO; i++) {
+        for (j = 0; j < TAMANHO; j++) {
+            tabuleiro[i][j] = AGUA;
         }
     }
 
-    // Posiciona navio vertical (tamanho 3)
-    for (i = 0; i < 3; i++) {
-        if (linha_vertical + i < 10 &&
-            tabuleiro[linha_vertical + i][coluna_vertical] == 0) {
-            tabuleiro[linha_vertical + i][coluna_vertical] = 3;
-        }
-    }
+    // Posiciona navio 1 - horizontal (linha 1, colunas 2 a 4)
+    tabuleiro[0][1] = NAVIO;
+    tabuleiro[0][2] = NAVIO;
+    tabuleiro[0][3] = NAVIO;
 
-    // Exibe cabeçalho com letras das colunas (A até J)
-    printf("   "); // Espaço inicial para alinhar com números das linhas
-    for (char letra = 'A'; letra <= 'J'; letra++) {
-        printf("%c ", letra);
+    // Posiciona navio 2 - vertical (coluna 6, linhas 5 a 7)
+    tabuleiro[4][5] = NAVIO;
+    tabuleiro[5][5] = NAVIO;
+    tabuleiro[6][5] = NAVIO;
+
+    // Posiciona navio 3 - diagonal principal (de [2][2] até [4][4])
+    tabuleiro[2][2] = NAVIO;
+    tabuleiro[3][3] = NAVIO;
+    tabuleiro[4][4] = NAVIO;
+
+    // Posiciona navio 4 - diagonal secundária (de [1][7] até [3][5])
+    tabuleiro[1][7] = NAVIO;
+    tabuleiro[2][6] = NAVIO;
+    tabuleiro[3][5] = NAVIO;
+
+    // Imprime identificação das colunas (A a J)
+    printf("   ");
+    for (i = 0; i < TAMANHO; i++) {
+        printf(" %c", 'A' + i);
     }
     printf("\n");
 
-    // Exibe o tabuleiro com numeração das linhas (1 até 10)
-    for (int linha = 0; linha < 10; linha++) {
-        printf("%2d ", linha + 1); // Mostra o número da linha
-        for (int coluna = 0; coluna < 10; coluna++) {
-            printf("%d ", tabuleiro[linha][coluna]);
+    // Imprime o tabuleiro com identificação das linhas (1 a 10)
+    for (i = 0; i < TAMANHO; i++) {
+        printf("%2d ", i + 1);  // Número da linha
+        for (j = 0; j < TAMANHO; j++) {
+            printf(" %d", tabuleiro[i][j]);
         }
         printf("\n");
     }
